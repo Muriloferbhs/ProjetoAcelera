c0b7d9760d91b612bca0d7da26767e4e6c393916
Murilo Fernando Barbosa Hummel da Silva <Muriloferbhs@gmail.com>
2025-10-29 11:22:39 -0300

    Add files via upload

---
+++ StudyFlow/Pergunta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace StudyFlow
+{
+    internal class Pergunta
+    {
+        [JsonPropertyName("ID")] public string ID {  get; set; }
+        [JsonPropertyName("Tipo")] public string Tipo { get; set; }
+        [JsonPropertyName("Pontuacao")] public int Pontuacao {  get; set; }
+        [JsonPropertyName("Tema")] public string Tema {  get; set; }
+        [JsonPropertyName("Area")] public string Area {  get; set; }
+        [JsonPropertyName("Enunciado")] public string Enunciado {  get; set; }
+        [JsonPropertyName("A")] public string A {  get; set; }
+        [JsonPropertyName("B")] public string B {  get; set; }
+        [JsonPropertyName("C")] public string C {  get; set; }
+        [JsonPropertyName("D")] public string D {  get; set; }
+        [JsonPropertyName("E")] public string E {  get; set; }
+        [JsonPropertyName("Resposta")] public string Resposta {  get; set; }
+
+        
+    }
+}
